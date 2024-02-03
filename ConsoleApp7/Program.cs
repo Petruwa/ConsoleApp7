f54123fb8d17c1d3ac53eb283dc4d9ec1e3561d9
Petruwa <151304491+Petruwa@users.noreply.github.com>
2024-02-03 10:10:50 +0800

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class HospitalWorker
+{
+    public string Position { get; set; }
+    public int Age { get; set; }
+
+    public virtual void DisplayInfo()
+    {
+        Console.WriteLine($"Position: {Position}");
+        Console.WriteLine($"Age: {Age}");
+    }
+}
+
+class Nurse : HospitalWorker
+{
+    public override void DisplayInfo()
+    {
+        Console.WriteLine("Nurse:");
+        base.DisplayInfo();
+    }
+}
+
+class Surgeon : HospitalWorker
+{
+    public override void DisplayInfo()
+    {
+        Console.WriteLine("Surgeon:");
+        base.DisplayInfo();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        HospitalWorker[] workers = new HospitalWorker[4];
+
+        workers[0] = new HospitalWorker { Position = "General Hospital Worker", Age = 25 };
+        workers[1] = new Nurse { Position = "RN Nurse", Age = 30 };
+        workers[2] = new Surgeon { Position = "Orthopedic Surgeon", Age = 40 };
+        workers[3] = new Nurse { Position = "LPN Nurse", Age = 27 };
+
+        foreach (var worker in workers)
+        {
+            worker.DisplayInfo();
+            Console.WriteLine();
+        }
+    }
+}
